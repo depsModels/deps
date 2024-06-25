5c1eee3aa9e626c10259718e2a43a35d2e6b5ffc
MiguelLewandowski <miguelstrapazonl@gmail.com>
2024-06-25 19:21:21 -0300

    form

--- src/js/responsive.js
+++ src/js/responsive.js
@@ -1,10 +1,15 @@
 function checkWidth() {
     const elements = document.querySelectorAll('.titles-end');
+    const titles = document.querySelectorAll('.titles');
+    const label_who_we_are = document.querySelectorAll('.label-who-we-are');
     if (window.innerWidth < 767) {
         elements.forEach(element => element.classList.remove('titles-end'));
+        titles.forEach(element => element.classList.add('text-center'));
+        label_who_we_are.forEach(element => element.style.fontSize = '3.2vh');
     } else {
-        // Opcional: Adiciona a classe novamente se a largura for maior que 767 pixels
         elements.forEach(element => element.classList.add('titles-end'));
+        titles.forEach(element => element.classList.remove('text-center'));
+        label_who_we_are.forEach(element => element.style.fontSize = '');
     }
 }
 
