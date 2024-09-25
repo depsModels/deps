30265246877c0a870831a10658edb331b92a8fbc
MiguelLewandowski <miguelstrapazonl@gmail.com>
2024-09-25 18:38:07 -0300

    fix:menu centralizando os elementos e erro menu hamburguer corrigido

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,10 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll(".links");
 
   let menuOpen = false;
-
   menuToggle.addEventListener("click", function () {
-    menuToggle.classList.toggle("active");
-
     if (!menuOpen) {
       menu.style.display = "flex";
       requestAnimationFrame(() => {
@@ -16,17 +13,41 @@ document.addEventListener("DOMContentLoaded", function () {
           setTimeout(() => {
             link.style.opacity = "1";
             link.style.transform = "translateY(0)";
+            link.style.pointerEvents = "auto"; // Habilita eventos de clique
           }, index * 100);
         });
       });
+  
+      // Habilita eventos de clique no botão do modal
+      const buttonNav = document.getElementById("button-nav");
+      buttonNav.style.pointerEvents = "auto";
+  
       menuOpen = true;
     } else {
-      menu.classList.remove("show");
-      links.forEach((link) => {
-        link.style.opacity = "0";
-        link.style.transform = "translateY(20px)";
-      });
-      menuOpen = false;
+      // Verifica se o modal está aberto
+      const modal = document.getElementById("contatoModal");
+      if (modal.classList.contains("show")) {
+        // Fecha o modal
+        modal.classList.remove("show");
+        const modalDialog = modal.querySelector(".modal-dialog");
+        modalDialog.classList.remove("show");
+      }
+  
+      // Fecha o menu após um atraso
+      setTimeout(() => {
+        menu.classList.remove("show");
+        links.forEach((link) => {
+          link.style.opacity = "0";
+          link.style.transform = "translateY(20px)";
+          link.style.pointerEvents = "none"; // Desabilita eventos de clique
+        });
+  
+        // Desabilita eventos de clique no botão do modal
+        const buttonNav = document.getElementById("button-nav");
+        buttonNav.style.pointerEvents = "none";
+  
+        menuOpen = false;
+      }, 200);
     }
   });
 
@@ -35,14 +56,20 @@ document.addEventListener("DOMContentLoaded", function () {
       menuToggle.classList.remove("active");
       menu.style.display = "flex";
       menuOpen = false;
-
+  
       // Reset link styles
       links.forEach((link) => {
         link.style.opacity = "1";
         link.style.transform = "translateY(0)";
+        link.style.pointerEvents = "auto"; // Habilita eventos de clique
       });
     } else {
       menu.style.display = "none"; // Esconde o menu original em telas menores
+      links.forEach((link) => {
+        link.style.opacity = "0";
+        link.style.transform = "translateY(20px)";
+        link.style.pointerEvents = "none"; // Desabilita eventos de clique
+      });
     }
   }
 
