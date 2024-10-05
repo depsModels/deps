4d36ff19f0d3b3676d38e0420ec06a659f8bc34a
Raul Lize Teixeira <lizeraul1@gmail.com>
2024-10-05 14:16:33 -0300

    fix: faq
    Cards do faq foram ajustados para terem a mesma largura, e foi organizado brevemente o responsivo da mesma section

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,5 @@ import "/src/js/form.js";
 import "/src/js/links.js";
 import "/src/js/menu.js";
 import "/src/js/responsive.js";
+import "/src/js/faq.js";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
