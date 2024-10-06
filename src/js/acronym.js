73cbc883e40b4a08e7bbf479173999b50838386b
MiguelLewandowski <miguelstrapazonl@gmail.com>
2024-10-06 16:57:06 -0300

    fix: cards height na classe card-features

--- src/js/acronym.js
+++ src/js/acronym.js
@@ -10,80 +10,75 @@ let sectionSimplicity = document.querySelector("#simplicity");
 
 let pointer = document.querySelector(".pointer");
 
-radioDesign.addEventListener("mouseover", (e) => {
-  sectionDesign.style.display = "block";
-  sectionEfficiency.style.display = "none";
-  sectionProductivity.style.display = "none";
-  sectionSimplicity.style.display = "none";
-});
-
-radioEfficiency.addEventListener("mouseover", (e) => {
-  sectionDesign.style.display = "none";
-  sectionEfficiency.style.display = "block";
-  sectionProductivity.style.display = "none";
-  sectionSimplicity.style.display = "none";
-});
-
-radioProductivity.addEventListener("mouseover", (e) => {
-  sectionDesign.style.display = "none";
-  sectionEfficiency.style.display = "none";
-  sectionProductivity.style.display = "block";
-  sectionSimplicity.style.display = "none";
-});
-
-radioSimplicity.addEventListener("mouseover", (e) => {
-  sectionDesign.style.display = "none";
-  sectionEfficiency.style.display = "none";
-  sectionProductivity.style.display = "none";
-  sectionSimplicity.style.display = "block";
-});
-
-// Gerenciamento da classe "active" nas labels
 const depsAcronym = document.querySelector(".deps-acronym");
 const labels = depsAcronym.querySelectorAll("p");
 
-// Função para remover a classe 'active' de todas as labels
 const removeActiveFromAll = () => {
   labels.forEach((label) => {
     label.classList.remove("active");
   });
 };
 
-// Adicionar comportamento para manter a classe 'active'
-labels.forEach((label) => {
-  label.addEventListener("mouseover", () => {
-    // Remove 'active' de todas as labels
-    removeActiveFromAll();
+function desktopFunction() {
+  console.log("Modo desktop ativado");
 
-    label.classList.add("active");
+  // Mostrar full-word em todas as labels no modo desktop
+  labels.forEach((label) => {
+    const fullWord = label.querySelector("span.full-word");
+    fullWord.style.opacity = "1"; // Mostrar full-word
+    fullWord.style.transform = "translateY(0)"; // Resetar a posição
   });
-});
 
+  radioDesign.addEventListener("mouseover", (e) => {
+    sectionDesign.style.display = "block";
+    sectionEfficiency.style.display = "none";
+    sectionProductivity.style.display = "none";
+    sectionSimplicity.style.display = "none";
+  });
 
-// Verifica se a tela é menor que 768px para aplicar as funcionalidades móveis
-if (window.innerWidth <= 768) {
-  // Ativar a primeira label e exibir sua descrição ao carregar
+  radioEfficiency.addEventListener("mouseover", (e) => {
+    sectionDesign.style.display = "none";
+    sectionEfficiency.style.display = "block";
+    sectionProductivity.style.display = "none";
+    sectionSimplicity.style.display = "none";
+  });
+
+  radioProductivity.addEventListener("mouseover", (e) => {
+    sectionDesign.style.display = "none";
+    sectionEfficiency.style.display = "none";
+    sectionProductivity.style.display = "block";
+    sectionSimplicity.style.display = "none";
+  });
+
+  radioSimplicity.addEventListener("mouseover", (e) => {
+    sectionDesign.style.display = "none";
+    sectionEfficiency.style.display = "none";
+    sectionProductivity.style.display = "none";
+    sectionSimplicity.style.display = "block";
+  });
+}
+
+function mobileFunction() {
+  console.log("Modo mobile ativado");
+  
   radioDesign.classList.add("active");
   sectionDesign.style.display = "block";
 
-  // Adicionar evento de clique nas labels
   labels.forEach((label) => {
     label.addEventListener("click", () => {
-      // Remove 'active' de todas as labels
+
       removeActiveFromAll();
 
-      // Adiciona 'active' à label clicada
       label.classList.add("active");
 
-      // Alterna a exibição da palavra completa
       labels.forEach((lbl) => {
         const fullWord = lbl.querySelector("span.full-word");
         if (lbl !== label) {
-          fullWord.style.opacity = "0";
-          fullWord.style.transform = "translateY(10px)";
+          fullWord.style.opacity = "0"; // Esconder full-word
+          fullWord.style.transform = "translateY(10px)"; // Mover a palavra para baixo
         } else {
-          fullWord.style.opacity = "1";
-          fullWord.style.transform = "translateY(0)";
+          fullWord.style.opacity = "1"; // Mostrar full-word
+          fullWord.style.transform = "translateY(0)"; // Resetar a posição
         }
       });
 
@@ -105,3 +100,17 @@ if (window.innerWidth <= 768) {
     });
   });
 }
+
+function checkWindowSize() {
+  if (window.innerWidth <= 768) { 
+    mobileFunction();
+  } else {
+    desktopFunction();
+  }
+}
+
+// Verifica o tamanho da janela quando a página carrega
+window.onload = checkWindowSize;
+
+// Verifica o tamanho da janela toda vez que ela for redimensionada
+window.onresize = checkWindowSize;
