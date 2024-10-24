070f9e7407249b8d8ab295eee7323040749b3c1e
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2024-10-24 15:37:40 -0300

    fix: style inline removido

--- src/js/form.js
+++ src/js/form.js
@@ -1,30 +1,34 @@
-// Adiciona um ouvinte de evento para o envio do formulário
-document.getElementById("myForm").addEventListener("submit", function (event) {
-  event.preventDefault(); // Impede o envio padrão do formulário
+// Função para lidar com o envio de formulário via AJAX
+function handleFormSubmit(formId) {
+  document.getElementById(formId).addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
 
-  // Captura os dados do formulário
-  var formData = new FormData(this);
+    // Captura os dados do formulário
+    var formData = new FormData(this);
 
-  // Envia os dados do formulário via AJAX para o arquivo PHP
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "sendform.php", true);
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      // Exibe a mensagem retornada pelo PHP no modal
-      showModal(xhr.responseText);
-      // Recarrega a página após 3 segundos
-      setTimeout(function () {
-        location.reload();
-      }, 3000);
-    }
-  };
-  xhr.send(formData);
-});
+    // Envia os dados do formulário via AJAX para o arquivo PHP
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "sendform.php", true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // Exibe a mensagem retornada pelo PHP no modal
+        showModal(xhr.responseText);
+        // Recarrega a página após 3 segundos
+        setTimeout(function () {
+          location.reload();
+        }, 3000);
+      }
+    };
+    xhr.send(formData);
+  });
+}
 
-// Função para exibir o modal com a mensagem e definir a cor do modal
+// Aplica a função para ambos os formulários
+handleFormSubmit("myForm"); // Formulário do footer
+handleFormSubmit("modalForm"); // Formulário do modal
+
+// Função para exibir o modal com a mensagem
 function showModal(message) {
   document.getElementById("modalMessage").innerText = message;
-  var modal = document.getElementById("myModal");
-
   $("#myModal").modal("show");
 }
