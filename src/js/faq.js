93358b7e15a36aea4957b25ee674a93fbe49eea7
Raul Lize Teixeira <114193996+Raullize@users.noreply.github.com>
2025-06-03 15:45:10 -0300

    chore: remove todo o código legado, preparando o ambiente de desenvolvimento para a versão v2.0.0

--- src/js/faq.js
+++
@@ -1,7 +0,0 @@
-const faqQuestions = document.querySelectorAll(".faq-question");
-
-faqQuestions.forEach((question) => {
-  question.addEventListener("click", () => {
-    question.parentElement.classList.toggle("ativo");
-  });
-});
